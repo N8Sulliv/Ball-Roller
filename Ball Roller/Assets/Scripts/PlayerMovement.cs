08dc57e8b675fc80c279fb8cfc22054f758e0766
NSParadox <76891137+NSParadox@users.noreply.github.com>
2021-02-08 13:00:19 -0600

    Tutorial Complete

---
+++ Ball Roller/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 800.0f;
+    public Text scoreText;
+    private int count = 0;
+
+    public Text winText;
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+
+        if (count >= 15)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            scoreText.text = "Score: " + count;
+        }
+    }
+}
